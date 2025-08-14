652b1c2f4d1ee5101deea6c31f4432d2787a7247
shashankkafle <shashank.kaphle@gmail.com>
2025-08-14 11:12:43 +0545

    Add traffic generation and duration calculation to test script
    - Integrated UniversalTrafficGenerator to generate route files.
    - Imported necessary functions for configuration and duration calculation.
    - Set up traffic generation loop to create multiple route files with different seeds.
    - Calculated fixed durations for generated routes based on configuration settings.

--- TLCS/test.py
+++ TLCS/test.py
@@ -1,5 +1,21 @@
 from dotenv import load_dotenv
 import os
+from fixed_duration_calculation import get_durations 
+from utils import import_train_configuration, set_sumo, set_train_path
+from universal_generator import UniversalTrafficGenerator
 
+# This is a test script just to test code. It is like a rough notebook.
 load_dotenv(override=True)
-print(f"route_file after load_dotenv: {os.getenv('OUTPUT_TRIPS_FILE')}")
+config = import_train_configuration(config_file='training_settings.ini')
+output_trips_file = "D:/q-earning/dqn-intersection-agent/TLCS/intersection/test_route.rou.xml"
+NET_FILE = os.getenv("NET_FILE")
+TrafficGen = UniversalTrafficGenerator(
+    NET_FILE,
+    output_trips_file,
+    sim_end=config['max_steps'],
+    vehicle_count=config['n_cars_generated']
+)
+for i in range(10):
+    print("Generating route file for seed", i)
+    TrafficGen.generate_routefile(seed=i)
+    fixed_durations = get_durations(output_trips_file, config['max_steps'])
